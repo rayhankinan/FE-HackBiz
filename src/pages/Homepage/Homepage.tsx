b55efbf953d28863ca750dd4e5cd63bb31e677f1
VincentiusV <vincentiusverel@gmail.com>
2022-07-29 18:25:42 +0700

    Update Homepage.tsx

--- src/pages/Homepage/Homepage.tsx
+++ src/pages/Homepage/Homepage.tsx
@@ -7,7 +7,7 @@ export const Homepage: React.FC = () => {
 
     return (
         <StandardLayout>
-            <div className='bg-gradient-to-b from-slate-900 via-slate-600 to-white '>
+            <div className='bg-gradient-to-b from-slate-900 via-slate-600 to-white min-h-screen'>
                 <h1 className="site-layout-background font-sans font-bold text-white text-9xl text-center" style={{ padding: 24, minHeight: 380 }}>
                     Better Planning <br/> Less Waste
                 </h1>
