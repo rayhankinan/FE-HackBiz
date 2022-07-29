159af11976c5b090bc8dd05a2814efb0d044d73e
fikfikriii <18220097@std.stei.itb.ac.id>
2022-07-30 01:15:18 +0700

    Update Login.tsx

--- src/pages/Users/Login.tsx
+++ src/pages/Users/Login.tsx
@@ -26,46 +26,59 @@ export const Login: React.FC = () => {
     }
 
     return (
-        <Spin tip="Loading . . ." spinning={loading}>
-            <Form
-                name="login"
-                className="max-w-xs"
-                initialValues={{ remember: true }}
-                onFinish={onFinish}
-            >
-                <Form.Item
-                    name="username"
-                    rules={[{ required: true, message: 'Please input your Username!' }]}
-                >
-                    <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
-                </Form.Item>
-                <Form.Item
-                    name="password"
-                    rules={[{ required: true, message: 'Please input your Password!' }]}
-                >
-                    <Input
-                        prefix={<LockOutlined className="site-form-item-icon" />}
-                        type="password"
-                        placeholder="Password"
-                    />
-                </Form.Item>
-                <Form.Item>
-                    <Form.Item name="remember" valuePropName="checked" noStyle>
-                        <Checkbox>Remember me</Checkbox>
-                    </Form.Item>
+        < div className='bg-gradient-to-b from-slate-900 via-slate-600 to-white min-h-screen'>
 
-                    <a className="float-right" onClick={() => navigate('/recover')}>
-                        Forgot password
-                    </a>
-                </Form.Item>
+            <div className='flex items-center justify-center h-auto space-x-40 pt-56'>
+                <div className="shadow-lg rounded-2xl w-96 bg-white p-4" style={{ height: 520 }}>
+                    <img src="../../../src/resources/logo.png" alt="" style={{ display: 'block', marginLeft: 'auto', marginRight: 'auto', width: '40%' }} />
+                    <div className=" text-black font-sans text-center">
+                        <p className="text-4xl font-medium mt-4 mb-7 ">
+                            Log In
+                        </p>
+                    </div>
+                    <Spin tip="Loading . . ." spinning={loading}>
+                        <Form
+                            name="login"
+                            className="max-w-xs"
+                            initialValues={{ remember: true }}
+                            onFinish={onFinish}
+                        >
+                            <Form.Item
+                                name="username"
+                                rules={[{ required: true, message: 'Please input your Username!' }]}
+                            >
+                                <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
+                            </Form.Item>
+                            <Form.Item
+                                name="password"
+                                rules={[{ required: true, message: 'Please input your Password!' }]}
+                            >
+                                <Input
+                                    prefix={<LockOutlined className="site-form-item-icon" />}
+                                    type="password"
+                                    placeholder="Password"
+                                />
+                            </Form.Item>
+                            <Form.Item>
+                                <Form.Item name="remember" valuePropName="checked" noStyle>
+                                    <Checkbox>Remember me</Checkbox>
+                                </Form.Item>
 
-                <Form.Item>
-                    <Button type="primary" htmlType="submit" className="w-full">
-                        Log in
-                    </Button>
-                    Or <a onClick={() => navigate('/register')}>register now!</a>
-                </Form.Item>
-            </Form>
-        </Spin>
+                                <a className="float-right" onClick={() => navigate('/recover')}>
+                                    Forgot password
+                                </a>
+                            </Form.Item>
+
+                            <Form.Item>
+                                <Button type="primary" htmlType="submit" className="w-full text-black">
+                                    Log in
+                                </Button>
+                                Or <a onClick={() => navigate('/register')}>register now!</a>
+                            </Form.Item>
+                        </Form>
+                    </Spin>
+                </div>
+            </div>
+        </div>
     )
 }
